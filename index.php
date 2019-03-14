d8e5aa93191915db20814ad95b3850935c28881d
dhimas yudangga <34560775+dhimasya@users.noreply.github.com>
2019-03-14 19:43:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,18 +2,23 @@
 $servername = "localhost";
 $username = "id8972136_root";
 $password = "root001";
+$database = "id8972136_akademik";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-echo "Connected successfully";
+$customers = $conn->query("SELECT * FROM customer");
+if ($customers->num_rows > 0) {
+    while($row = $customers->fetch_assoc()) {
+        echo "$row[nama] ($row[gender])<br>$row[alamat]<br>";
+    }
+}
 
 if ($conn){
     $conn->close();
-    echo "Connection closed";
 }
